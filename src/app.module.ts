91fad1fd473882578527f3aea8176a719ab6e267
vanda-mugo <johnmugo006@gmail.com>
2025-09-30 22:28:32 +0300

    added config module for handling .env files

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,20 +6,26 @@ import { TweetsModule } from './tweets/tweets.module';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProfileModule } from './profile/profile.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     UsersModule,
     TweetsModule,
     AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true, // this will make the ConfigModule available globally
+    }),
     TypeOrmModule.forRootAsync({
-      useFactory: () => ({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
         type: 'postgres',
-        host: 'localhost',
-        port: 5432,
-        username: 'postgres',
-        password: '13881388Vanda',
-        database: 'nestjs',
+        host: configService.get('DATABASE_HOST'),
+        port: +configService.get('DATABASE_PORT'),
+        username: configService.get('DATABASE_USERNAME'),
+        password: configService.get('DATABASE_PASSWORD'),
+        database: configService.get('DATABASE_NAME'),
         autoLoadEntities: true, // this will automatically load entities that are registered through the forFeature() method
         // alternatively you can specify the entities here
         //entities: [User, Profile], // add your entities here to make typeorm aware of them
